4768f896364750a3866e16a530d4a0d708fdcc58
martapanc <marta.panc@gmail.com>
2017-10-05 22:48:13 +0100

    working multiple flags thingy

--- stubforcw2/mbaxtmp2_cw2/wc.py
+++ stubforcw2/mbaxtmp2_cw2/wc.py
@@ -4,9 +4,10 @@
 # 0       1     2       3     ...
 # lines  words   bytes   file.txt
 
-#TODO: Add "Try 'wc --help' for more information."
-#TODO: improve to accept flags in the form "-mcl..."
+#OK - TODO: Add "Try 'wc --help' for more information."
+#OK - TODO: improve to accept flags in the form "-mcl..."
 #TODO: fix the bloody encoding thing
+#TODO: reduce things a bit
 
 import sys
 
@@ -17,17 +18,18 @@ def wc():
     else:
         allValidFlags=True
         fileList = []
-        flagList = []
+        flagList = set([])
 
         for param in sys.argv[1:]:
             if param[0] == '-':
                 curFlag = param[1:]
-                if checkFlag(curFlag):
-                    flagList.append(curFlag)
-                else:
-                    allValidFlags = False
-                    print(invalidOption(curFlag))
-                    exit()
+                for char in curFlag: #check cases with more flags. Eg: -wcl
+                    if checkFlag(char):
+                        flagList.add(char)
+                    else:
+                        allValidFlags = False
+                        print(invalidOption(char))
+                        exit()
             else:
                 fileList.append(param)
         if allValidFlags :
@@ -99,7 +101,7 @@ def checkFlag(argument):
         'l': True,
         'w': True,
 #        'm': True,
-        'w': True
+        'c': True
     }
     return switcher.get(argument, False)
 
@@ -135,44 +137,4 @@ def countChars(file):
     file.seek(0)
     return char_count
 
-def numbers_to_strings(argument):
-    switcher = {
-        0: "zero",
-        1: "one",
-        2: "two",
-    }
-    return switcher.get(argument, invalidOption(argument))
-
-def miniWc():
-    args_len = len(sys.argv)
-    if args_len == 2:
-        file = sys.argv[1]
-    elif args_len > 2:
-        print("Only one file as argument.")
-        exit()
-    else:
-        print("No file argument was inserted.")
-        exit()
-
-    byte_count = 0
-    word_count = 0
-    line_count = 0
-
-    try:
-        with open(file, 'r', encoding='utf8') as f:
-            for line in f:
-                words = line.split()
-                line_count += 1
-                word_count += len(words)
-                byte_count += len(line.encode("utf8"))
-            print(" " +str(line_count) + " " + str(word_count) + " " + str(byte_count) + " " + file)
-    except FileNotFoundError:
-        print("Error: file \"" + file + "\" not found.")
-    #except UnicodeDecodeError:
-       # print("Error: cannot read file \"" + file + "\" correctly.")
-
 wc()
-
-#miniWc()
-
-#implement the no argument case
