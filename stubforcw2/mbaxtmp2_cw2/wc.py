70fe80c57df6a751ec21d3d91ee8e8bd31efb359
martapanc <marta.panc@gmail.com>
2017-10-05 23:12:37 +0100

    reduced size a bit

--- stubforcw2/mbaxtmp2_cw2/wc.py
+++ stubforcw2/mbaxtmp2_cw2/wc.py
@@ -32,7 +32,8 @@ def wc():
                         exit()
             else:
                 fileList.append(param)
-        if allValidFlags :
+
+        if allValidFlags:
             if len(fileList) == 0 :
                 print ("*stdin not implemented yet*")
             else:
@@ -41,60 +42,63 @@ def wc():
                 #totalCharCount = 0
                 totalByteCount = 0
 
-                if len(flagList) == 0:
-                    for file in fileList:
-                        try:
-                            with open(file, 'r', encoding='utf8') as f:
+                # if len(flagList) == 0:
+                #     for file in fileList:
+                #         try:
+                #             with open(file, 'r', encoding='utf8') as f:
+                #                 line_count = countLines(f)
+                #                 word_count = countWords(f)
+                #                 byte_count = countBytes(f)
+                #                 if len(fileList) > 1:
+                #                     totalLineCount += line_count
+                #                     totalWordCount += word_count
+                #                     totalByteCount += byte_count
+                #                 print ("  " + str(line_count) + "\t" + str(word_count) + "\t" + str(byte_count) + "\t" + file)
+                #         except FileNotFoundError:
+                #             print("wc: " + file + ": No such file or directory")
+                #     if len(fileList) > 1:
+                #         print("  " + str(totalLineCount) + "\t" + str(totalWordCount) + "\t" + str(totalByteCount) + "\t total")
+                # else:
+                doLines = True if "l" in flagList else False
+                doWords = True if "w" in flagList else False
+                doBytes = True if "c" in flagList else False
+                #doChars = True if "m" in flagList else False
+
+                if len(flagList) == 0: #no arguments = -wcl
+                    doLines = True
+                    doWords = True
+                    doBytes = True
+
+                for file in fileList:
+                    try:
+                        with open(file, 'r', encoding='utf8') as f:
+                            if doLines:
                                 line_count = countLines(f)
+                                print(" " + str(line_count), end='')
+                            if doWords:
                                 word_count = countWords(f)
+                                print(" " + str(word_count), end='')
+                            # if doChars:
+                            #     char_count = countBytes(f)
+                            #     print(" " + str(char_count), end='')
+                            if doBytes:
                                 byte_count = countBytes(f)
-                                if len(fileList) > 1:
-                                    totalLineCount += line_count
-                                    totalWordCount += word_count
-                                    totalByteCount += byte_count
-                                print ("  " + str(line_count) + "\t" + str(word_count) + "\t" + str(byte_count) + "\t" + file)
-                        except FileNotFoundError:
-                            print("wc: " + file + ": No such file or directory")
-                    if len(fileList) > 1:
-                        print("  " + str(totalLineCount) + "\t" + str(totalWordCount) + "\t" + str(totalByteCount) + "\t total")
-                else:
-                    doLines = True if "l" in flagList else False
-                    doWords = True if "w" in flagList else False
-                    doBytes = True if "c" in flagList else False
-                    #doChars = True if "m" in flagList else False
+                                print(" " + str(byte_count), end='')
+                            print(" " + file)
 
-                    for file in fileList:
-                        try:
-                            with open(file, 'r', encoding='utf8') as f:
-                                if doLines:
-                                    line_count = countLines(f)
-                                    print(" " + str(line_count), end='')
-                                if doWords:
-                                    word_count = countWords(f)
-                                    print(" " + str(word_count), end='')
-                                # if doChars:
-                                #     char_count = countBytes(f)
-                                #     print(" " + str(char_count), end='')
-                                if doBytes:
-                                    byte_count = countBytes(f)
-                                    print(" " + str(byte_count), end='')
-                                print(" " + file)
-
-                                if len(fileList) > 1:
-                                    if doLines: totalLineCount += line_count
-                                    if doWords: totalWordCount += word_count
-                                    # if doChars: totalCharCount += char_count
-                                    if doBytes: totalByteCount += byte_count
-
-                                #print ("  " + str(line_count) + "\t" + str(word_count) + "\t" + str(byte_count) + "\t" + file)
-                        except FileNotFoundError:
-                            print("wc: " + file + ": No such file or directory")
-                    if len(fileList) > 1:
-                        if doLines: print(" " + str(totalLineCount), end='')
-                        if doWords: print(" " + str(totalWordCount), end='')
-                        #if doChars: print(" " + str(totalCharCount), end='')
-                        if doBytes: print(" " + str(totalByteCount), end='')
-                        print("\t total")
+                            if len(fileList) > 1:
+                                if doLines: totalLineCount += line_count
+                                if doWords: totalWordCount += word_count
+                                # if doChars: totalCharCount += char_count
+                                if doBytes: totalByteCount += byte_count
+                    except FileNotFoundError:
+                        print("wc: " + file + ": No such file or directory")
+                if len(fileList) > 1:
+                    if doLines: print(" " + str(totalLineCount), end='')
+                    if doWords: print(" " + str(totalWordCount), end='')
+                    #if doChars: print(" " + str(totalCharCount), end='')
+                    if doBytes: print(" " + str(totalByteCount), end='')
+                    print("\t total")
 
 def checkFlag(argument):
     switcher = {
@@ -133,7 +137,7 @@ def countBytes(file):
 def countChars(file):
     char_count = 0
     for line in file:
-        char_count += len(line.encode("utf8"))
+        char_count += len(line)
     file.seek(0)
     return char_count
 
