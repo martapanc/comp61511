986c0d5f5e6a1a2fbc7b9e478f823eee1cee7ca5
Marta Pancaldi <martapancaldi@Martas-MacBook-Air.local>
2017-10-05 19:02:43 +0100

    trying the multi params and flags

--- stubforcw2/mbaxtmp2_cw2/wc.py
+++ stubforcw2/mbaxtmp2_cw2/wc.py
@@ -2,6 +2,7 @@
 
 #python3 wc.py <flags> <files>
 # 0       1     2       3     ...
+# lines  words   bytes   file.txt
 
 #TODO: Add "Try 'wc --help' for more information."
 #TODO: improve to accept flags in the form "-mcl..."
@@ -26,13 +27,26 @@ def wc():
                 else:
                     allValidFlags = False
                     print(invalidOption(curFlag))
+                    exit()
             else:
                 fileList.append(param)
-        if allValidFlags and len(fileList) != 0 :
-            print (sys.argv[1:])
-            print (fileList)
-        else:
-            print ("*stdin not implemented yet*")
+        if allValidFlags :
+            if len(fileList) == 0 :
+                print ("*stdin not implemented yet*")
+            else:
+                print (sys.argv[1:])
+                print (fileList)
+
+                if flagList == 0:
+                    print("no flags") #compute all 3 values
+                else:
+
+
+                    if "-l" in flagList:
+                        line_count = count
+
+
+
 
             #print(param)
 
@@ -51,7 +65,25 @@ def checkFlag(argument):
 def invalidOption(arg):
     return "wc: invalid option -- '" + str(arg) + "'"
 
+def countLines(file):
+    line_count = 0
+    for line in file:
+        line_count += 1
+    return line_count
 
+def countWords(file):
+    word_count = 0
+    for line in file:
+        words = line.split()
+        word_count += len(words)
+    return word_count
+
+def countBytes(file):
+    byte_count = 0
+    for line in file:
+        words = line.split()
+        byte_count += len(line.encode("utf8"))
+    return byte_count
 
 def numbers_to_strings(argument):
     switcher = {
