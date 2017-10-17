d40112cb89b0da17faa9a752ee499871417a9cd1
martapanc <marta.panc@gmail.com>
2017-10-17 22:44:17 +0100

    working cw with argparse, missing cases with -- and '-wcg'

---
+++ CW3/compare.sh
@@ -0,0 +1,6 @@
+
+echo " * wc_argparse.py:"
+python3 wc_argparse.py ${@:1}
+echo ""
+echo " * wc:"
+wc ${@:1}
--- CW3/square.py
+++ CW3/square.py
@@ -1,13 +1,24 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display square of a given number",
-    type=int)
-parser.add_argument("-v", "--verbosity", help="increase output verbosity", type=int, choices=[0,1,2])
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-v", "--verbose", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
+#parser.add_argument("square", type=int, help="display square of a given number")
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+#parser.add_argument("-v", "--verbosity", help="increase output verbosity", action="count", default=0)
 args = parser.parse_args()
-answer= args.square**2
-if args.verbosity == 2:
-    print("the square of {} equals {}".format(args.square, answer))
-elif args.verbosity == 1:
-    print("{}^2 == {}".format(args.square, answer))
-else: print(answer)
+answer= args.x**args.y
+if args.quiet:
+    print(answer)
+elif args.verbose:
+    print("{} to the power {} equals {}".format(args.x, args.y, answer))
+else:
+     print("{}^{} == {}".format(args.x, args.y, answer))
+#if args.verbosity >= 2:
+#    print("running '{}'".format(__file__))
+#if args.verbosity >= 1:
+#    print("{}^{} == {}".format(args.x, args.y, answer))
+#else:
+#    print(answer)
