12d0217cb659d979ad1f49829b08dfbf476ea11f
martapanc <marta.panc@gmail.com>
2017-10-22 19:04:48 +0100

    Added version, help and long and short flags

--- CW4/compare.sh
+++ CW4/compare.sh
@@ -1,9 +1,10 @@
 
-echo " * wc_argparse.py:"
-python3 wc_argparse.py -wcmlL ${@:1}
-echo
+#echo " * wc_argparse.py:"
+#python3 wc_argparse.py -wcmlL ${@:1}
+#echo
 echo " * wc_unit.py:"
-python3 wc_unit.py -wcmlL ${@:1}
+#python3 final_wc.py -wcmlL ${@:1}
+python3 final_wc.py  ${@:1}
 echo
 # echo " * wc2:"
 # python wc2.py ${@:1}
@@ -12,4 +13,5 @@ echo
 # python wc3.py ${@:1}
 # echo
 echo " * wc:"
-wc -wcmlL ${@:1}
+#wc -wcmlL ${@:1}
+wc ${@:1}
---
+++ CW4/final_wc.py
@@ -0,0 +1,239 @@
+# Created by mbaxtmp2 on 4/10/17
+
+# python3 wc.py <flags> <files>
+# 0       1     2       3     ...
+# lines  words   bytes   file.txt
+
+import sys
+
+
+def wc():
+    args_len = len(sys.argv)
+    if args_len < 2:
+        not_implem()
+    else:
+        file_list = []
+        flag_list = []
+        flags_are_valid = True
+
+        flags_are_valid, flag_list, file_list = all_valid_args(sys.argv[1:])
+
+        print (flag_list)
+        if flags_are_valid:
+            print(compute_result(flag_list, file_list))
+
+
+def compute_result(flag_list, file_list):
+    if len(file_list) == 0:
+        not_implem()
+    else:
+        resultString = ''
+        total_line_count = 0
+        total_word_count = 0
+        total_byte_count = 0
+        total_char_count = 0
+        total_max_line_count = 0
+
+        do_lines = True if "l" in flag_list else False
+        do_words = True if "w" in flag_list else False
+        do_bytes = True if "c" in flag_list else False
+        do_chars = True if "m" in flag_list else False
+        do_max_line = True if "L" in flag_list else False
+
+        if len(flag_list) == 0:  # no flags = -wcl
+            do_lines = True
+            do_words = True
+            do_bytes = True
+
+        for file in file_list:
+            try:
+                with open(file, 'r', encoding='utf8') as f:
+                    if do_lines:
+                        line_count = count_lines(f)
+                        resultString += "\t" + str(line_count)
+                    if do_words:
+                        word_count = count_words(f)
+                        resultString += "\t" + str(word_count)
+                    if do_chars:
+                        char_count = count_chars(f)
+                        resultString += "\t" + str(char_count)
+                    if do_bytes:
+                        byte_count = count_bytes(f)
+                        resultString += "\t" + str(byte_count)
+                    if do_max_line:
+                        max_count = get_max_line(f)
+                        resultString += "\t" + str(max_count)
+
+                    resultString += "\t" + file + "\n"
+
+                    if len(file_list) > 1:
+                        if do_lines: total_line_count += line_count
+                        if do_words: total_word_count += word_count
+                        if do_chars: total_char_count += char_count
+                        if do_bytes: total_byte_count += byte_count
+                        if do_max_line:
+                            if total_max_line_count < max_count:
+                                total_max_line_count = max_count
+
+            except FileNotFoundError:
+                resultString += "wc: " + file + ": No such file or directory\n"
+            except IsADirectoryError:
+                resultString += "wc: " + file + ": Is a directory\n" # replicate wc's behaviour if one of the args is a directory - ugly, I know
+                if do_lines: resultString += "\t0"
+                if do_words: resultString += "\t0"
+                if do_chars: resultString += "\t0"
+                if do_bytes: resultString += "\t0"
+                if do_max_line: resultString += "\t0"
+                resultString += "\t" + file + "\n"
+        if len(file_list) > 1:
+            if do_lines: resultString += "\t" + str(total_line_count)
+            if do_words: resultString += "\t" + str(total_word_count)
+            if do_chars: resultString += "\t" + str(total_char_count)
+            if do_bytes: resultString += "\t" + str(total_byte_count)
+            if do_max_line: resultString += "\t" + str(total_max_line_count)
+            resultString += "\ttotal\n"
+        return resultString
+
+
+def check_flag(flag):
+    #if flag == '-':
+    #    invalid(flag)
+    #    return False
+    validFlags = ['l', 'w', 'c', 'm', 'L']
+    if not flag.isalpha():
+        return False
+    if flag in validFlags:
+        return True
+    else:
+        return False
+
+
+def check_long_flag(flag):
+#    valid_flags = ['bytes', 'chars', 'lines', 'max-line-length', 'words', 'help', 'version']
+    valid_flags = {'bytes': 'c', 'chars': 'm', 'lines': 'l', 'max-line-length': 'L', 'words': 'w',
+                    'c':'c', 'w':'w', 'm':'m', 'l':'l', 'L':'L',
+                    'help': 'help', 'version': 'version', 'h':'help', 'v':'version'}
+    if not flag.isalpha():
+        return False
+    if flag in valid_flags:
+        return True, valid_flags[flag]
+    else:
+        return False
+
+def all_valid_args(args):
+    file_list = []
+    flag_list = set([])
+    for param in args:
+        if len(param)>2 and param[:2] == '--':
+            if check_long_flag(param[2:]):
+                var, short_flag = check_long_flag(param[2:])
+                if (short_flag == "version"):
+                    version()
+                elif (short_flag == "help"):
+                    show_help()
+                else:
+                    flag_list.add(short_flag)
+            else:
+                unrecognized(param)
+        elif param == '--':
+            not_implem()
+        elif param[0] == '-' and len(param)>1:
+            cur_flag = param[1:]
+            for cf in cur_flag:  # check cases with more flags. E.g.: -wc -cl -lwc
+                if check_flag(cf):
+                    flag_list.add(cf)
+                else:
+                    invalid(cf)
+                    #return False, {}, []
+        else:
+            file_list.append(param)  # If not preceded by "-", handle it as a file
+    return True, flag_list, file_list
+
+def count_lines(file):
+    line_count = 0
+    for line in file:
+        if "\n" in line:
+            line_count += 1
+    file.seek(0)  # restores pointer at the beginning of file
+    return line_count
+
+def count_words(file):
+    word_count = 0
+    for line in file:
+        words = line.split()
+        word_count += len(words)
+    file.seek(0)
+    return word_count
+
+def count_bytes(file):
+    byte_count = 0
+    for line in file:
+        byte_count += len(line.encode("utf-8"))
+    file.seek(0)
+    return byte_count
+
+def count_chars(file):
+    char_count = 0
+    for line in file:
+        char_count += len(line)
+    file.seek(0)
+    return char_count
+
+def get_max_line(file):
+    max_count = 0
+    for line in file:
+        line_length = len(line)
+        # line_length=0
+        # for char in line:
+        #     if char == '\s':
+        #         line_length += 6
+        #     else:
+        #         line_length +=1
+
+        if line_length > max_count:
+            max_count = line_length-1
+    file.seek(0)
+    return max_count
+
+
+def invalid(option):
+    sys.exit("wc: invalid option -- '" + str(option) + "'\nTry 'wc --help' for more information.")
+
+def unrecognized(option):
+    sys.exit("wc: unrecognized option '" + str(option) + "'\nTry 'wc --help' for more information.")
+
+def not_implem():
+    sys.exit("*stdin not implemented yet*")
+
+def version():
+    sys.exit("wc.py 4.0 - Python implementation of\n"
+             + "  \"wc (GNU coreutils) 8.25\n"
+             + "  Copyright (C) 2016 Free Software Foundation, Inc.\n"
+             + "  Written by Paul Rubin and David MacKenzie.\"\n\n"
+             + "Created by Marta Pancaldi\n"
+             + "Final project of the course \'Software Engineering Concepts in Practice\' 2017 \n  by Dr Bijan Parsia @ The University of Manchester\n\n"
+             + "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n"
+             + "This is free software: you are free to change and redistribute it.\n"
+             + "There is NO WARRANTY, to the extent permitted by law.\n")
+
+def show_help():
+    sys.exit("Usage: wc [OPTION]... [FILE]...\nor:  wc [OPTION]... --files0-from=F\n"
+    + "Print newline, word, and byte counts for each FILE, and a total line if\nmore than one FILE is specified.  A word is a non-zero-length sequence of"
+    + "characters delimited by white space. \n\n"
+    + "With no FILE, or when FILE is -, read standard input.\n\n"
+    + "The options below may be used to select which counts are printed, always in\nthe following order: newline, word, character, byte, maximum line length.\n"
+    + "  -c, --bytes            print the byte counts\n"
+    + "  -m, --chars            print the character counts\n"
+    + "  -l, --lines            print the newline counts\n"
+    + "      --files0-from=F    read input from the files specified by\n"
+    + "                           NUL-terminated names in file F;\n"
+    + "                           If F is - then read names from standard input\n"
+    + "  -L, --max-line-length  print the maximum display width\n"
+    + "  -w, --words            print the word counts \n"
+    + "      --help     display this help and exit\n"
+    + "      --version  output version information and exit\n\n"
+    + "GNU coreutils online help: <http://www.gnu.org/software/coreutils/>\nFull documentation at: <http://www.gnu.org/software/coreutils/wc>\n"
+    + "or available locally via: info '(coreutils) wc invocation'")
+
+if __name__ == "__main__":
+   wc()
